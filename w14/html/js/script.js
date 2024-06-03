2b030aea9f6eb5b50f81a0efb96ade36780ec447
JunHee Han <39343280+ganari3518@users.noreply.github.com>
2024-06-03 16:52:57 +0900

    feat : add w14

---
+++ w14/html/js/script.js
@@ -0,0 +1,37 @@
+function discordButton(){
+    window.location.href = "https://discord.gg/U7veAPA9";
+}
+
+function gitHubButton(){
+    window.location.href = "https://github.com/ganari3518";
+}
+
+function docsButton(){
+    window.location.href = "./src/index.html"
+}
+
+var Body = {
+    setColor : function(color){
+        document.querySelector('body').style.color = color;
+    },
+
+    setBackgroundColor : function(color){
+        document.querySelector('body').style.backgroundColor = color;
+    }
+}
+
+  $(function () {
+    $("#change_unit_helper").on("click", function () {
+      if ($(this).is(":checked")) {
+        Body.setBackgroundColor('black');
+        Body.setColor('white');
+        NightDayButton.setValue('day');
+        Links.setColor('powderblue');
+      } else {
+        Body.setBackgroundColor('white');
+        Body.setColor('black');
+        NightDayButton.setValue('night');
+        Links.setColor('blue');
+      }
+    });
+  });
